9044c7ac1080179687b9e1138766c23d7d9b11ef
Frédéric <aubergerebirth@gmail.com>
2026-03-08 22:01:50 +0400

    Mise à jour

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,6 @@
 
 export default new Map([
-["src/content/articles/ce-que-la-balance-ne-dit-pas-v4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-que-la-balance-ne-dit-pas-v4.mdx&astroContentModuleFlag=true")],
+["src/content/articles/ce-que-la-balance-ne-dit-pas.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-que-la-balance-ne-dit-pas.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/ce-quon-fait-avec-ce-quon-sait-final.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-quon-fait-avec-ce-quon-sait-final.mdx&astroContentModuleFlag=true")],
-["src/content/articles/l-un-saisit-l-autre-contient.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fl-un-saisit-l-autre-contient.mdx&astroContentModuleFlag=true")],
-["src/content/articles/ce-que-la-balance-ne-dit-pas.mdx.download/ce-que-la-balance-ne-dit-pas.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-que-la-balance-ne-dit-pas.mdx.download%2Fce-que-la-balance-ne-dit-pas.mdx&astroContentModuleFlag=true")],
-["src/content/articles/ce-que-la-balance-ne-dit-pas.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-que-la-balance-ne-dit-pas.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/l-un-saisit-l-autre-contient.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fl-un-saisit-l-autre-contient.mdx&astroContentModuleFlag=true")]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://syncognie.pages.dev',
+  integrations: [mdx(), sitemap()]
 });
