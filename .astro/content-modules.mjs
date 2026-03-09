dcc14143be92ff28f29d6ce4111212381d6a303a
Frédéric <aubergerebirth@gmail.com>
2026-03-09 21:37:31 +0400

    Dialogue 2

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,5 +2,8 @@
 export default new Map([
 ["src/content/articles/l-un-saisit-l-autre-contient.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fl-un-saisit-l-autre-contient.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/dialogue-01-est-ce-quon-choisit.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdialogue-01-est-ce-quon-choisit.mdx&astroContentModuleFlag=true")],
-["src/content/articles/ce-quon-fait-avec-ce-quon-sait-final.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-quon-fait-avec-ce-quon-sait-final.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/ce-quon-fait-avec-ce-quon-sait-final.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-quon-fait-avec-ce-quon-sait-final.mdx&astroContentModuleFlag=true")],
+["src/content/articles/dialogue-02.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdialogue-02.mdx&astroContentModuleFlag=true")],
+["src/content/articles/recit-elodie-21h47.mdx.download/recit-elodie-21h47.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-elodie-21h47.mdx.download%2Frecit-elodie-21h47.mdx&astroContentModuleFlag=true")],
+["src/content/articles/recit-elodie-21h47.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-elodie-21h47.mdx&astroContentModuleFlag=true")]]);
 		
