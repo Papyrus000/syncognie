6f007e82aa664b95fb0c3840951486c34ab69abf
Frédéric <aubergerebirth@gmail.com>
2026-03-12 09:55:52 +0400

    feat: activation de la sitemap

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
-["src/content/articles/ce-quon-attend-sans-savoir-quoi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-quon-attend-sans-savoir-quoi.mdx&astroContentModuleFlag=true")],
-["src/content/articles/dialogue-01-est-ce-quon-choisit.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdialogue-01-est-ce-quon-choisit.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/dialogue-02.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdialogue-02.mdx&astroContentModuleFlag=true")],
+["src/content/articles/dialogue-01-est-ce-quon-choisit.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fdialogue-01-est-ce-quon-choisit.mdx&astroContentModuleFlag=true")],
+["src/content/articles/ce-quon-attend-sans-savoir-quoi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-quon-attend-sans-savoir-quoi.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/rayan-ce-quon-cherche.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frayan-ce-quon-cherche.mdx&astroContentModuleFlag=true")],
-["src/content/articles/recit-elodie-21h47.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-elodie-21h47.mdx&astroContentModuleFlag=true")],
-["src/content/articles/recit-nathalie-6h14.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-nathalie-6h14.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/recit-nathalie-6h14.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-nathalie-6h14.mdx&astroContentModuleFlag=true")],
+["src/content/articles/recit-elodie-21h47.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-elodie-21h47.mdx&astroContentModuleFlag=true")]]);
 		
