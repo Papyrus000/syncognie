27234fb3ea48e67914de2d8e4a571f5e6dc63a33
Frédéric <aubergerebirth@gmail.com>
2026-03-13 14:01:42 +0400

    Mise à jour

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -8,5 +8,6 @@ export default new Map([
 ["src/content/articles/rayan-ce-quon-cherche.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frayan-ce-quon-cherche.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/gottman-couple-attachement-desir.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fgottman-couple-attachement-desir.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/recit-elodie-21h47.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-elodie-21h47.mdx&astroContentModuleFlag=true")],
-["src/content/articles/recit-nathalie-6h14.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-nathalie-6h14.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/recit-nathalie-6h14.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frecit-nathalie-6h14.mdx&astroContentModuleFlag=true")],
+["src/content/articles/ce-qu-on-sait-et-ce-qu-on-fait.mdx.download/ce-qu-on-sait-et-ce-qu-on-fait.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fce-qu-on-sait-et-ce-qu-on-fait.mdx.download%2Fce-qu-on-sait-et-ce-qu-on-fait.mdx&astroContentModuleFlag=true")]]);
 		
